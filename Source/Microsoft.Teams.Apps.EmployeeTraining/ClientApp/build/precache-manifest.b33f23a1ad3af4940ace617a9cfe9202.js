ac7a7c8b1f68ead07115f69d79b21a54a7c38dbd
syedjalal12 <syedjalal125@gmail.com>
2022-06-09 19:13:42 +0500

    Base Files

---
+++ Source/Microsoft.Teams.Apps.EmployeeTraining/ClientApp/build/precache-manifest.b33f23a1ad3af4940ace617a9cfe9202.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b679aadc22701818a3f6bf9e0f660aef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ef663bad5f5fa802388",
+    "url": "/static/css/2.9a766daa.chunk.css"
+  },
+  {
+    "revision": "b52ad2805c7c374c1cc7",
+    "url": "/static/css/main.3dd0822a.chunk.css"
+  },
+  {
+    "revision": "0ef663bad5f5fa802388",
+    "url": "/static/js/2.9afd5e63.chunk.js"
+  },
+  {
+    "revision": "856a5afbbaa9ea405195ec98340038fa",
+    "url": "/static/js/2.9afd5e63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b52ad2805c7c374c1cc7",
+    "url": "/static/js/main.8e0b56e2.chunk.js"
+  },
+  {
+    "revision": "3dc6fb61c0ef4d1924bf",
+    "url": "/static/js/runtime-main.ce1dec2b.js"
+  }
+]);
